fd8ccb7f4967861876de5db280f694e180c5981e
Enrique De Nobrega <edenobrega@sky.com>
2022-04-19 16:04:48 +0000

    Add db tables to admin page

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Facility, Tag, FacilityTag, TimeSlot, Booking
 
 # Register your models here.
+admin.site.register(Facility)
+admin.site.register(Tag)
+admin.site.register(FacilityTag)
+admin.site.register(TimeSlot)
+admin.site.register(Booking)
