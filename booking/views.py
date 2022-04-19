a0855d2b4e543ca62a48446b7138fdffc70c2d8a
Enrique De Nobrega <edenobrega@sky.com>
2022-04-19 16:07:03 +0000

    Create base template, create urls and view for index

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
+
+def get_booking_index(request):
+    return render(request, 'booking/index.html')
