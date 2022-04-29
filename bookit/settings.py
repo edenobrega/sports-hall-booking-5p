4f4aa55d3b5e69e798d80d220bc18e81da72d46f
Enrique De Nobrega <edenobrega@sky.com>
2022-04-29 01:47:11 +0000

    Make changes for deployment

--- bookit/settings.py
+++ bookit/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("APP_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['bookit-5p.herokuapp.com', 'localhost']
 
