b86a3261ce806e928b69bcd70a4ae211883fb556
Enrique De Nobrega <edenobrega@sky.com>
2022-05-26 07:56:48 +0000

    Update requirements.txt, add crispy-forms to settings.py

--- bookit/settings.py
+++ bookit/settings.py
@@ -45,7 +45,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'booking',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
