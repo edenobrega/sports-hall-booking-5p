951bb4db5dab3d1a5e37a80fee5d64b06f1bfd3a
Enrique De Nobrega <edenobrega@sky.com>
2022-05-26 15:37:29 +0000

    Update debug environment variable

--- bookit/settings.py
+++ bookit/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("APP_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = ['bookit-5p.herokuapp.com', 'localhost']
 
